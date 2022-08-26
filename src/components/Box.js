4ff1d449d29124a9978b2dfa8a088138630faa14
MatiasDBrites <matias.brites@gmail.com>
2022-08-26 11:03:36 -0500

    Archivos up

---
+++ src/components/Box.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+function Box(props) {
+
+  const [count, updateCount] = useState(0); 
+
+  function click() {
+    props.boxClicked(props.id);
+    updateCount (count +1);
+  }
+
+  return (
+    <div onClick={() => click()}>
+      <p>Box {props.id}</p>
+      <p>Contador Hijo: {count}</p>
+      <p>Diferencia con mi hermano: {count - props.brotherCount} </p>
+    </div>
+  );
+}
+
+export default Box
